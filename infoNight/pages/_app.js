2d5a58041f67a69f78251d4c5cd0ab6a72e42f03
Arthur Ponty <arthur.ponty76@gmail.com>
2021-12-03 05:16:05 +0100

    Scss du backoffice

--- infoNight/pages/_app.js
+++ infoNight/pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react"
 import '../styles/globals.scss'
+import '../styles/backoffice.scss'
 import Layout from "../components/Layout";
 import {Router} from 'next/dist/client/router'
 import NProgress from 'nprogress';
