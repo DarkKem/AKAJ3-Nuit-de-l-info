069d94b3ad09dedd1216b107e1e13dc87b363c8b
john-btg <john.batagne@epitech.eu>
2021-12-03 04:36:36 +0100

    push

--- infoNight/pages/backoffice.js
+++ infoNight/pages/backoffice.js
@@ -3,27 +3,41 @@ import {server} from "../config/index"
 
 
 export default function backoffice({data}) {
-
+    async function traite(id) {
+            let request = `${server}api/backoffice`
+            let serverRes = await fetch(request, {
+                method: "DELETE",
+                body: JSON.stringify({
+                    id: id,
+                })
+            })
+            if (serverRes.status === 200) {
+                alert("Demande supprimé")
+            }           
+    }
     return (
         <div>
             <h1>Admin</h1>
             {data.length>0 ? data.map((elem,key)=>(
                 <div key={key}>
-                    <h1>{elem.titre}</h1>
-                    <h3>{elem.categorie}</h3>
-                    <p>{elem.contenu}   </p>
-                    <button >accepter</button>
-                    <button>refuser</button>
+                    <h3>{elem.titre}</h3>
+                    <h4>{elem.categorie}</h4>
+                    <p>{elem.contenu}  </p>
+                    
+                    <button onClick={()=>{traite(elem.id)}}>Traité</button>
                     </div>
 
-            )):NULL}
+            )):null}
+            <div style={{width:"100%", height:"250px"}}>
+            <iframe style={{border: "0", overflow: "hidden"}}
+             frameBorder="0" height="100%" width="100%"
+              src="https://nuit-info-2021.toucantoco.com/embed.html?id=58aad1e4-62d5-439f-af08-cee332bc76b0&stage=staging">
+              </iframe></div>
         </div>
     );
 };
 
-
 export const getServerSideProps = async (context) => {
-    console.log('ici');
     const json = await fetch(`${server}api/backoffice`)
     let resJson= await json.json()
     return {props:{data:resJson.data}}
